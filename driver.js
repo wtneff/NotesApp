17919c872002fff1bd9f611e44dfd7f680bfb921
Tanner Neff <wneff.fr@gmail.com>
2019-04-07 12:03:00 -0400

    Add existing file

--- driver.js
+++ driver.js
@@ -1,32 +1,104 @@
-var note =[];
-
-// function getNote() {
-//     note = document.getElementById("note-form").onsubmit();
-//     return;
-// };
-
-function readNote() {
-    //note.push("hello");
-    storedText = localStorage.getItem('note', note);
-    console.log(storedText);
-}
-
-function reduceNote() {
-    note.splice(-1, 1);
-    console.log(note);
-}
+var noteArr = [];
+var whereAt = 0;
 
 function storeNote() {
-    var text = document.getElementById("note").value;
-    note.push(text);
-    localStorage.setItem('note', note);  
-    
-    //localStorage.setItem(note);
-};
+  var j = {};
 
-// function readNote() {
-//     localStorage.getItem(note);
-// };
+  j.note = document.getElementById("note").value;
+
+  var jJSON = JSON.stringify(j);
+  //var noteTxt = jJSON;
+  //noteTxt = document.getElementById("note").value;
+
+  //console.log(j);
+
+  noteArr.push(jJSON);
+  console.log(noteArr);
+  count();
+
+  //localStorage.note = noteArr;
+
+  //localStorage.setItem(note);
+}
+
+function readNote(whereAt) {
+  var i = 0;
+  if (whereAt > 0) {
+    i = whereAt;
+  }
+  var readTxt = JSON.parse(noteArr[i]);
+  if (whereAt <= 0) {
+    document.getElementById("message").innerHTML = "There are no notes!";
+  } else {
+    document.getElementById("message").innerHTML = readTxt.note;
+    console.log(readTxt.note);
+    count();
+  }
+}
+
+function clearNote() {
+  if (noteArr.length > 1) {
+    noteArr.splice(whereAt, 1);
+  } else {
+    noteArr.splice(0, 1);
+  }
+  console.log(noteArr);
+  count();
+  readNote(whereAt);
+}
+
+// function clearNote() {
+//     noteArr.
+//   document.getElementById("message").innerHTML = "";
+//   count();
+// }
+
+function count() {
+  if (noteArr.length == 0) {
+    document.getElementById("count").innnerHTML =
+      "There are no notes in the list!";
+  } else if (noteArr.length === 1) {
+    document.getElementById("count").innerHTML =
+      "There is one note in the list:";
+  } else {
+    document.getElementById("count").innerHTML =
+      "There are " + noteArr.length + " notes in the list:";
+
+    //   if (noteArr.length === 0) { "no notes."
+    //     } else {
+    //         " notes and you are on note: " +
+    //         whereAt + 1);
+    //     }
+  }
+}
+
+function next() {
+  whereAt = whereAt + 1;
+  if (noteArr[whereAt] == undefined) {
+    document.getElementById("message").innerHTML = "No more notes!";
+  } else {
+    readNote(whereAt);
+  }
+  //for (i = 0; i > noteArr.length; i++) {
+  //  readNote(noteArr[i]);
+  //}
+}
+
+function previous() {
+  if (whereAt == 0) {
+    readNote(whereAt);
+  } else {
+    whereAt = whereAt - 1;
+  }
+  if (noteArr[whereAt] == undefined) {
+    document.getElementById("message").innerHTML = "Error!";
+  } else {
+    readNote(whereAt);
+  }
+  //   for (i = 0; i > noteArr.length; i--) {
+  //     readNote(noteArr[i]);
+  //   }
+}
 
 // function clearNote() {
 //     localStorage.removeItem(note);
